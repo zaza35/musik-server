98b52e5b725269a9f10a54e319fd828fef32839d
Zaid ALHADAD <zaid172009@gmail.com>
2016-11-28 13:34:11 +0100

    preview in front

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,83 @@ var searchResult = new Vue({
         }
     }
 });
+
+var playingItem;
+$(document).ready(function () {
+    $("#player").prop({
+        volume: 0
+    });
+    var audioplayer = document.getElementById("player");
+    $("body").on("click", ".preview", function (e) {
+        e.preventDefault();
+        if (playingItem == undefined || playingItem.attr("link") != $(this).attr("link")) {
+            playingItem = $(this);
+            $("#player").animate({
+                volume: 0
+            }, 1000, function () {
+                $("#player").attr({
+                    'src': playingItem.attr("link")
+                });
+                audioplayer.play();
+                itemPreviewIcon("play");
+            });
+        } else {
+            if (audioplayer.paused == true) {
+                audioplayer.play();
+                itemPreviewIcon("play");
+            } else {
+                $("#player").animate({
+                    volume: 0
+                }, 1000, function () {
+                    audioplayer.pause();
+                    itemPreviewIcon("stop");
+                });
+            }
+        }
+
+    });
+    $("#player").on("loadeddata", function () {
+        itemPreviewIcon("played");
+        $("#player").animate({
+            volume: 1
+        }, 1000);
+    });
+    $("#player").on("ended", function () {
+        itemPreviewIcon("stop");
+    });
+});
+
+function itemPreviewIcon(action) {
+    switch (action) {
+        case "play":
+            $(".preview")
+                .removeClass('playingPreviewIcon')
+                .delay(300)
+                .children("i")
+                .removeClass('fa-spinner fa-pulse fa-stop')
+                .addClass('fa-volume-up');
+            playingItem
+                .addClass('playingPreviewIcon')
+                .children("i")
+                .removeClass('fa-volume-up fa-stop')
+                .addClass('playingPreviewIcon fa-spinner fa-pulse');
+
+            break;
+        case "played":
+            playingItem.children("i")
+                .removeClass('fa-spinner fa-pulse ')
+                .addClass('fa-stop');
+
+            break;
+        case "stop":
+            playingItem
+                .removeClass('playingPreviewIcon')
+                .delay(300)
+                .children("i")
+                .removeClass('fa-spinner fa-pulse fa-stop')
+                .addClass('fa-volume-up');
+            break;
+
+
+    }
+}
